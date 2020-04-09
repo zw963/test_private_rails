dd6a10fd5c1355f8f4a11b18a00bee6348740cff
Billy.Zheng <vil963@gmail.com>
2020-04-09 13:28:59 +0800

    Update travis-ci to support pg.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_04_09_044841) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
