e6da62cfe90e700fe7dcca48a7df3ad5bb1b699e
Billy.Zheng <vil963@gmail.com>
2020-04-09 12:50:08 +0800

    Add user model and test

---
+++ test/models/user_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  test "should create a new user" do
+    assert_equal 0, User.count
+    User.create(name: 'billy')
+    assert_equal 1, User.count
+  end
+end
