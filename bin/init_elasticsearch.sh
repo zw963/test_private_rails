d8aa131cedf6271ee22805197684bcb20b5339a7
Billy.Zheng <vil963@gmail.com>
2020-07-19 00:05:32 +0800

    Update

--- bin/init_elasticsearch.sh
+++ bin/init_elasticsearch.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo add-apt-repository "deb https://artifacts.elastic.co/packages/7.x/apt stable main"
-sudo apt-get update
-sudo apt-get install -q -y --no-install-recommends elasticsearch
-sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install analysis-kuromoji
-
-# if [ -f /usr/share/elasticsearch/bin/elasticsearch ]; then
-#     sudo useradd elasticsearch
-#     sudo mkdir -p /var/log/elasticsearch /var/lib/elasticsearch
-#     sudo chown -R elasticsearch:elasticsearch /var/log/elasticsearch /var/lib/elasticsearch
-# else
-#     wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-#     sudo add-apt-repository "deb https://artifacts.elastic.co/packages/7.x/apt stable main"
-#     sudo apt-get update
-#     sudo apt-get install -q -y --no-install-recommends elasticsearch
-#     sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install analysis-kuromoji
-# fi
+if [ -f /usr/share/elasticsearch/bin/elasticsearch ]; then
+    sudo useradd elasticsearch
+    sudo mkdir -p /var/log/elasticsearch /var/lib/elasticsearch
+    sudo chown -R elasticsearch:elasticsearch /var/log/elasticsearch /var/lib/elasticsearch
+else
+    wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+    sudo add-apt-repository "deb https://artifacts.elastic.co/packages/7.x/apt stable main"
+    sudo apt-get update
+    sudo apt-get install -q -y --no-install-recommends elasticsearch
+    sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install analysis-kuromoji
+fi
 
 sudo chmod 0644 /etc/elasticsearch
-
 sudo systemctl start elasticsearch
