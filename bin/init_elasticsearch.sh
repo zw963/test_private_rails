0e21132a94d08dd49fef9bf93584ce64f6bcd894
Billy.Zheng <vil963@gmail.com>
2020-07-18 23:33:59 +0800

    Update

--- bin/init_elasticsearch.sh
+++ bin/init_elasticsearch.sh
@@ -10,7 +10,7 @@ sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install analysis-kuromoji
 # if [ -f /usr/share/elasticsearch/bin/elasticsearch ]; then
 #     sudo useradd elasticsearch
 #     sudo mkdir -p /var/log/elasticsearch /var/lib/elasticsearch
-#     sudo chown elasticsearch:elasticsearch /var/log/elasticsearch /var/lib/elasticsearch
+#     sudo chown -R elasticsearch:elasticsearch /var/log/elasticsearch /var/lib/elasticsearch
 # else
 #     wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 #     sudo add-apt-repository "deb https://artifacts.elastic.co/packages/7.x/apt stable main"
@@ -19,6 +19,6 @@ sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install analysis-kuromoji
 #     sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install analysis-kuromoji
 # fi
 
-sudo chown elasticsearch:elasticsearch /etc/elasticsearch
+sudo chown -R elasticsearch:elasticsearch /etc/elasticsearch
 
 sudo systemctl start elasticsearch
