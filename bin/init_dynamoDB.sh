6730719c69bfb4d4a75b511cb935df872ed4cb30
Billy.Zheng <vil963@gmail.com>
2020-07-18 22:34:50 +0800

    Update

--- bin/init_dynamoDB.sh
+++ bin/init_dynamoDB.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-set -x
 if [ ! -f ~/dynamodb_local_latest/DynamoDBLocal.jar ]; then
     mkdir -p ~/dynamodb_local_latest
     curl -sSL https://s3.ap-northeast-1.amazonaws.com/dynamodb-local-tokyo/dynamodb_local_latest.tar.gz --output ~/dynamodb_local_latest.tar.gz
     tar xzvf ~/dynamodb_local_latest.tar.gz -C ~/dynamodb_local_latest
 fi
+
 cd  ~/dynamodb_local_latest
-pwd ~/dynamodb_local_latest
+
 /usr/bin/java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -inMemory&
